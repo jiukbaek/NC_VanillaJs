7d2237a8073cfd9a8a73ab918118317c7c5e257b
JIUKBAEK <jiuk205@naver.com>
2019-07-18 19:27:13 +0900

    190718

---
+++ weather.js
@@ -0,0 +1,47 @@
+const API_KEY = "b6907d289e10d714a6e88b30761fae22";
+const COORDS = "coords";
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`);
+    
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS,JSON.stringify(coordsObj));
+}
+
+function handleGeoError(){
+    console.log("Geo Location Error");
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude,
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+    
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
